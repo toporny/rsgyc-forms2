7d5113de6d8a399812f04f08efe111c9208cccfa
toporny <toporny@tlen.pl>
2016-05-07 19:46:16 +0100

    validation for all fields = done

--- public_html/js/custom.js
+++ public_html/js/custom.js
@@ -2,45 +2,167 @@ var demoApp = angular.module('demoApp', ['valdr', 'pascalprecht.translate']);
   demoApp.config(function(valdrProvider) {
     valdrProvider.addConstraints({
       'Person': {
-        'childName': {'minLength': {
-            'number': 3,
-            'message': "message.minLength"
-          },
-          'maxLength': {
-            'number': 13,
-            'message': "message.maxLength"
-          },
-        },
-
-        'age': {
-          'required': {
-            'message': "message.required"
-          },
-        //   'digits': {
-        //     integer: 2,
-        //     fraction: 0,
-        //     message: 'message.number'
-        //   },
-        //   'max': {
-        //     value: '17',
-        //     message: 'Junior sailor must be max 17 years old.'
-        //   },
-        //   'min': {
-        //     value: '9',
-        //     message: 'Junior sailor must have at 9 years old.'
-        //   }
-        },
         'sailingModuleToComplete': {
           'required': {
             'message': 'Please select Sailing Module.'
           }
-        }
+        },      
+        'age': {
+          'required': {
+            'message': "message.required"
+          },
+        },
+        'boatType': {
+          'required': {
+            'message': "message.required"
+          },
+        },
+        'childName': {
+          'minLength': {
+            'number': 3,
+            'message': "message.minLength"
+          },
+          'pattern': {
+            'value': /^[\w\s.'-]+$/,
+            'message': "message.properName"
+          },
+          'maxLength': {
+            'number': 22,
+            'message': "message.maxLength"
+          }
+        },
+        'parentMobile': {
+          'required': {
+            'message': "message.required"
+          },
+          'maxLength': {
+            'number': 30,
+            'message': "message.maxLength"
+          },
+          'pattern': {
+            'value': /^[\d\s\+]+$/,
+            'message': "message.properMobile"
+          },
+        },
+        'parent2Mobile': {
+          'maxLength': {
+            'number': 30,
+            'message': "message.maxLength"
+          },
+          'pattern': {
+            'value': /^[\d\s\+]+$/,
+            'message': "message.properMobile"
+          },
+        },
+        'parentEmail': {
+          'required': {
+            'message': "message.required"
+          },
+          "email": {
+            "message": "Must be a valid E-Mail address."
+          }          
+        },
+        'parentName': {
+          'minLength': {
+            'number': 3,
+            'message': "message.minLength"
+          },
+          'pattern': {
+            'value': /^[\w\s.'-]+$/,
+            'message': "message.properName"
+          },
+          'maxLength': {
+            'number': 22,
+            'message': "message.maxLength"
+          }
+        },
+        'acceptTerms': {
+          'checkboxRequired': {
+            'value': true,
+            'message': "message.required"
+          },
+        },
+        'understandParkingPermit': {
+          'checkboxRequired': {
+            'value': true,
+            'message': "message.required"
+          },
+        },
+        'dateOfBirth': {
+          'required': {
+            'message': "message.required"
+          },
+          'pattern': {
+            'value': /^\d{2}\/\d{2}\/\d{4}$/,
+            'message': "message.birthdate"
+          }
+        },
+        'cardholdername': {
+          'required': {
+            'message': "message.required"
+          }
+        },
+        'cardnumber': {
+          'required': {
+            'message': "message.required"
+          },
+          'pattern': {
+            'value': /^\d.*$/,
+            'message': "message.digits"
+          }
+        },
+        'exp_month': {
+          'required': {
+            'message': "message.required"
+          },
+        },
+        'exp_year': {
+          'required': {
+            'message': "message.required"
+          },
+        },
+        'cvv': {
+          'required': {
+            'message': "message.required"
+          },
+          'pattern': {
+            'value': /^\d.*$/,
+            'message': "message.digits"
+          }
+        },
+
       }
     });
   });
 
 
- 
+
+
+demoApp.factory('checkboxRequired', function () {
+    return {
+      name: 'checkboxRequired', // this is the validator name that can be referenced from the constraints JSON
+      validate: function (value, constraint) {
+        // value: the value to validate
+        // constraint: the validator arguments
+        return value === constraint.value;
+      }
+    };  
+  // return {
+  //   name: 'checkboxRequired', // this is the validator name that can be referenced from the constraints JSON
+  //   validate: function (value, arguments) {
+  //     // console.log('value',value);
+  //     // console.log('arguments',arguments);
+  //     // value: the value to validate
+  //     // arguments: the validator arguments
+  //     return value === true;
+  //   }
+  // };
+});
+
+demoApp.config(function (valdrProvider) {
+  valdrProvider.addValidator('checkboxRequired');
+});
+
 
 
   demoApp.config(function (valdrMessageProvider, $translateProvider) {
@@ -49,15 +171,31 @@ var demoApp = angular.module('demoApp', ['valdr', 'pascalprecht.translate']);
     $translateProvider.translations('en', {
       'message': {
         'required'  : '{{fieldName}} is required.',
-//        'size'      : '{{fieldName}} must have minimum {{min}} and maximum {{max}} characters.',
+        //'size'      : '{{fieldName}} must have minimum {{min}} and maximum {{max}} characters.',
         'minLength' : '{{fieldName}} must have at least {{number}} characters.',
         'maxLength' : '{{fieldName}} must have maximum {{number}} characters.',
-//        'number'    : '{{fieldName}} must be a number.',
-//        'digits'    : '{{fieldName}} must be less {{max}} and more than {{min}}.'
+        'properName' : '{{fieldName}} - please type proper name.',
+        'birthdate' : '{{fieldName}} - wrong format.',
+        'digits' : '{{fieldName}} - only digits allowed.',
+        'properMobile' : 'Wrong {{fieldName}} format. Only digits allowed.'
       },
       'Person': {
-        'childName': 'Child Name',
-        'age': 'Child age'
+        'childName': 'Child name',
+        'parentName': 'Parent name',
+        'parentMobile': 'Parent Mobile Phone',
+        'parent2Mobile': 'Parent 2 Mobile Phone',
+        'parentEmail' : 'Email',
+        'boatType': 'Boat type',
+        'age': 'Age',
+        'dateOfBirth' : 'Birth date',
+        'acceptTerms' :'acceptTerms',
+        'understandParkingPermit': 'Parking Permit rules',
+        'cardholdername' : 'Cardholder name',
+        'cardnumber' : 'Card number',
+        'exp_month' : 'Expired month',
+        'exp_year' : 'Expired year',
+        'cvv' : 'CVV'
+
       }
     });
     // valdrMessageProvider.addMessages({
@@ -73,11 +211,10 @@ var demoApp = angular.module('demoApp', ['valdr', 'pascalprecht.translate']);
 
   demoApp.controller('TestController', function ($scope, valdr) {
     vm = this;
-//    $scope.person = {};
-    vm.aha = 'aha';    
-    // <select name="repeatSelect" id="repeatSelect" ng-model="data.repeatSelect">
-    //   <option ng-repeat="option in testController.genders" value="{{option.id}}">{{option.name}}</option>
-    // </select>
+    
+    vm.submit = submit;
+    vm.formSubm = false;
+
     vm.sailing_module = [  
       {id : '',  value: '-- select --'},
       {id : "Start Sailing" , value: "Start Sailing"},
@@ -91,31 +228,47 @@ var demoApp = angular.module('demoApp', ['valdr', 'pascalprecht.translate']);
       {id : "Adventure 1 (Course C &amp; D only)" , value: "Adventure 1 (Course C &amp; D only)"},
       {id : "Adventure 2 (Course C &amp; D only)" , value: "Adventure 2 (Course C &amp; D only)"}
     ];
-    sailingModuleToComplete_index = 0;
+    //sailingModuleToComplete_index = 0;
 
-    vm.possible_ages = [
-      {id : '',  value: '-- select --'},
-      {id : '9',  value: '9 years old'},
-      {id : '10', value: '10 years old'},
-      {id : '11', value: '11 years old'},
-      {id : '12', value: '12 years old'},
-      {id : '13', value: '13 years old'},
-      {id : '14', value: '14 years old'},
-      {id : '15', value: '15 years old'},
-      {id : '16', value: '16 years old'},
-      {id : '17', value: '17 years old'},
-      {id : '18', value: '18 years old'},
-    ];
-    vm.possible_ages_index = 0;
+    vm.possible_ages = [{id : '',  value: '-- select --'}];
+    for (i=9; i<19; i++) {
+      vm.possible_ages.push({id : i, value: i});
+    }
+
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    vm.months = [{id : '',  value: '- exp. month -'}];
+    for (i=0;i<12;i++) {
+      vm.months.push({id : i+1, value: '('+((i<9)?('0'+(i+1)):i+1)+') ' + monthNames[i]});
+    }
+
+    vm.year = [{id : '',  value: '- exp. year -'}];
+    for (i = new Date().getFullYear(); i<new Date().getFullYear()+25; i++) {
+      vm.year.push({id : i, value: i})
+    }
+
+    console.log('controler start');
+    //vm.form.$setPristine();
 
 
+    function submit() {
+      console.log('submit');
+      vm.formSubm = true;
+      
+    //ng-class="{ 'form-submitted': formSubmitted }"
 
-    vm.genders = [
-      { name: 'Male', id: 1 },
-      { name: 'Female', id: 2 }
-    ];
-    // $scope.constraints = valdr.getConstraints();
-    // $scope.showAllErrors = function () {
-    //     $scope.formSubmitted = true;
-    // }
+      //vm.constraints = valdr.getConstraints();
+      
+      vm.showAllErrors = function () {
+      }
+
+      if (vm.form.$valid) {
+        console.log('form valid');
+      }
+      else {
+        console.warn('form not valid');
+      }
+    }
+
+
   });
