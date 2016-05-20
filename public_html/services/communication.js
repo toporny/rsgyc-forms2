e4486f49fcf4609e8c013385d8a20c0cb662c580
Przemyslaw Rzeznik <przemyslaw.rzeznik@3vcorporate.com>
2016-05-20 17:16:20 +0100

    communication  ongoing..

--- public_html/services/communication.js
+++ public_html/services/communication.js
@@ -2,56 +2,26 @@
   angular.module('rsgycApp')
   .service("communication", communication);
 
-  communication.$inject = [ '$http', 'backendUrl' ];
+  communication.$inject = [ '$http', 'backendUrl' , '$httpParamSerializerJQLike'];
 
-  function communication ($http, backendUrl) {
+  function communication ($http, backendUrl, $httpParamSerializerJQLike) {
 
     function send(dataObj) {
       console.log('data to send',dataObj);
 
-    $http({
-        url: backendUrl,
-        method: "POST",
-        data: Object.toparams(dataObj),
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-    })
-    .then(function(response) {
-            // success
-    }, 
-    function(response) { // optional
-            // failed
-    });
+      $http({
+          url: backendUrl,
+          method: "POST",
+          data: $httpParamSerializerJQLike(dataObj),
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      })
+      .then(function(response) {
+              // success
+      }, 
+      function(response) { // optional
+              // failed
+      });
 
-
-// var username = 'test1';
-// var password = 'test1';
-
-//  var payload = 'j_username=' + username + '&j_password=' + password;
-//   var config = {
-//     headers: {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
-//   }
-//         var data = $.param({
-//             json: JSON.stringify(dataObj)
-//         });
-
-//   $http.post(backendUrl, dataObj, config)
-//   .success(function(data) {
-//     console.log('data',data)
-
-//   });
- 
-
-
-      // var res = $http
-      // .post(backendUrl, dataObj)
-      // .success(function(dataReturn, status, headers, config) {
-      //   console.log('data',dataReturn);
-      //   return dataReturn;
-      // })
-      // .error(function(dataObj, status, headers, config) {
-      //   console.warn( "failure message: " + JSON.stringify({dataObj: dataObj}));
-      //   return dataObj;
-      // });
     }
 
 
